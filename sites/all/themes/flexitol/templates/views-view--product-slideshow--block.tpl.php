87749d0abcb20a0c8f1a512f8aced50175ab6da4
Baldino Morelli <baldino.m@gmail.com>
2020-06-24 15:35:59 +1000

    Edited flexslider template in order to have unlimited videos

--- sites/all/themes/flexitol/templates/views-view--product-slideshow--block.tpl.php
+++ sites/all/themes/flexitol/templates/views-view--product-slideshow--block.tpl.php
@@ -1,49 +1,60 @@
 <?php
 if ($node = menu_get_object()) {
 
-//echo "<pre>";var_dump($node->field_product_video['und'][0]);echo "</pre>";die;
+//echo "<pre>";var_dump($node->field_product_video['und']);echo "</pre>";die;
 
-  if (!empty($node->field_product_video['und'][0]['video_url'])) {
+if (is_array($node->field_product_video['und']) && count($node->field_product_video['und']) > 0) {
 
-      $containerOpen = substr($rows, 0, strpos($rows, '<ul class="slides">'));
+  $containerOpen = substr($rows, 0, strpos($rows, '<ul class="slides">'));
 
-      $lis = substr($rows, strpos($rows, '<ul class="slides">'), strpos($rows, '</ul>'));
+  $lis = substr($rows, strpos($rows, '<ul class="slides">'), strpos($rows, '</ul>'));
 
-      $lis = str_replace(array('<ul class="slides">', '</ul>', "</div>"), '', $lis);
+  $lis = str_replace(array('<ul class="slides">', '</ul>', "</div>"), '', $lis);
 
-      $video_link = $node->field_product_video['und'][0]['video_url'];
+  foreach ($node->field_product_video['und'] as $video_arr) {
 
-      $video_html = "";
+    if (!empty($video_arr['video_url'])) {
 
-      if (preg_match("/youtube/i", $video_link)) {
-            $video_ID = substr($video_link, strpos($video_link, "?v=") + 3);
+        $video_link = $video_arr['video_url'];
 
-            $video_html = '<iframe class="reframe" width="1266" height="712" src="https://www.youtube.com/embed/'.$video_ID.'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-        }
+        $video_html = "";
 
-        //https://vimeo.com/86291629
-        //<iframe src="https://player.vimeo.com/video/86291629" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        if (preg_match("/youtube/i", $video_link)) {
+              $video_ID = substr($video_link, strpos($video_link, "?v=") + 3);
 
-        elseif (preg_match("/vimeo/i", $video_link)) {
-            $video_ID = substr($video_link, strpos($video_link, ".com/") + 5);
+              $video_html = '<iframe class="reframe" width="1266" height="712" src="https://www.youtube.com/embed/'.$video_ID.'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+          }
 
-            $video_html = '<iframe class="reframe" width="1266" height="712" src="https://player.vimeo.com/video/'.$video_ID.'" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
-        }
+          //https://vimeo.com/86291629
+          //<iframe src="https://player.vimeo.com/video/86291629" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 
-      $lis .= "<li class='video' data-thumb='".file_create_url($node->field_product_video['und'][0]['thumbnail_path'])."'><div>".$video_html."</div></li>";
+          elseif (preg_match("/vimeo/i", $video_link)) {
+              $video_ID = substr($video_link, strpos($video_link, ".com/") + 5);
 
-      $rows = $containerOpen.'<ul class="slides">'.$lis.'</ul></div>';
+              $video_html = '<iframe class="reframe" width="1266" height="712" src="https://player.vimeo.com/video/'.$video_ID.'" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+          }
 
-      //echo "<pre>";var_dump($rows);echo "</pre>";die;
+        $lis .= "<li class='video' data-thumb='".file_create_url($node->field_product_video['und'][0]['thumbnail_path'])."'><div>".$video_html."</div></li>";
+
+
+        //echo "<pre>";var_dump($rows);echo "</pre>";die;
+
+      }
+
+    }
+
+  $rows = $containerOpen.'<ul class="slides">'.$lis.'</ul></div>';
 
   }
 
 }
 ?>
 
-<?php print $rows; ?>
-<?php if ($footer): ?>
+<?php if (!empty($lis)) :
+
+print $rows;
+ if ($footer): ?>
     <div class="view-footer">
       <?php print $footer; ?>
     </div>
-<?php endif; ?>
+<?php  endif; endif;  ?>
--- sites/all/themes/flexitol/templates/views-view--product-slideshow-navigation--block.tpl.php
+++ sites/all/themes/flexitol/templates/views-view--product-slideshow-navigation--block.tpl.php
@@ -3,30 +3,44 @@ if ($node = menu_get_object()) {
 
 //echo "<pre>";var_dump($node->field_product_video['und'][0]);echo "</pre>";die;
 
-  if (!empty($node->field_product_video['und'][0]['video_url'])) {
+  if (is_array($node->field_product_video['und']) && count($node->field_product_video['und']) > 0) {
 
-      $containerOpen = substr($rows, 0, strpos($rows, '<ul class="slides">'));
+    $containerOpen = substr($rows, 0, strpos($rows, '<ul class="slides">'));
 
-      $lis = substr($rows, strpos($rows, '<ul class="slides">'), strpos($rows, '</ul>'));
+    $lis = substr($rows, strpos($rows, '<ul class="slides">'), strpos($rows, '</ul>'));
 
-      $lis = str_replace(array('<ul class="slides">', '</ul>', "</div>"), '', $lis);
+    $lis = str_replace(array('<ul class="slides">', '</ul>', "</div>"), '', $lis);
 
-      $video_link = $node->field_product_video['und'][0]['video_url'];
+    foreach ($node->field_product_video['und'] as $video_arr) {
 
-      $lis .= '<li><div class="views-field views-field-field-image-gallery"><div class="field-content"><img typeof="foaf:Image" class="img-responsive" src="'.file_create_url($node->field_product_video['und'][0]['thumbnail_path']).'" alt="" draggable="false"></div></div></li>';
+      if (!empty($video_arr['video_url'])) {
 
-      $rows = $containerOpen.'<ul class="slides">'.$lis.'</ul></div>';
+          $lis .= '<li><div class="views-field views-field-field-image-gallery"><div class="field-content"><img typeof="foaf:Image" class="img-responsive" src="'.file_create_url($video_arr['thumbnail_path']).'" alt="" draggable="false"></div></div></li>';
 
-      //echo "<pre>";var_dump($rows);echo "</pre>";die;
+
+
+          //echo "<pre>";var_dump($rows);echo "</pre>";die;
+
+      }
+
+    }
+
+    $rows = $containerOpen.'<ul class="slides">'.$lis.'</ul></div>';
 
   }
 
 }
-?>
 
-<?php print $rows; ?>
-<?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-<?php endif; ?>
+if (!empty($lis)) :
+
+  print $rows;
+
+  if ($footer): ?>
+      <div class="view-footer">
+        <?php print $footer; ?>
+      </div>
+<?php
+
+  endif;
+
+endif;
