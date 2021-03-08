ad860c0ac98571112a3cf77b2c1fe69ba76768a4
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2021-03-08 15:48:39 +0700

    Dev

--- dev.php
+++ dev.php
@@ -1,2 +1,10 @@
 <?php
 
+/*
+ssh u781-dokrqb835gyy@dermaltherapy.com.au -p18765
+
+git remote remove sg_dev
+git remote remove sg_live
+
+git remote add sg_dev ssh://u781-dokrqb835gyy@dermaltherapy.com.au:18765/home/u781-dokrqb835gyy/www/dev.dermaltherapy.com.au/dev.dermal.git
+git remote add sg_live ssh://u781-dokrqb835gyy@dermaltherapy.com.au:18765/home/u781-dokrqb835gyy/www/dermaltherapy.com.au/live.dermal.git
