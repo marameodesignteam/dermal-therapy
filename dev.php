db567cb8e981020696d390d8128aaecaa027a526
Cloudways <dev@marameodesign.com>
2022-05-19 03:07:01 +0000

    LIVE

--- dev.php
+++ dev.php
@@ -5,7 +5,7 @@ ssh master_pyebeyzefp@45.77.51.3
 
 === git flow ====
 LIVE
-ssh master_pyebeyzefp@45.77.51.3 -p 22 "cd /home/master/applications/gfsytsahed/public_html && git reset --hard && git pull origin master && git reset --hard && git log -n 5 && git status"
+ssh master_pyebeyzefp@45.77.51.3 -p 22 "cd /home/master/applications/gcnxtbedxc/public_html && git reset --hard && git pull origin master && git reset --hard && git log -n 5 && git status"
 
 
 
